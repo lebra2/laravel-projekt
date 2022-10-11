fb979ebc4be0798f61674d0367cc2d57032442ad
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-10-11 08:43:00 +0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Product/Product', [
+            'products' => Product::all(),
+        ]);
+    }
+}
---
+++ resources/js/Pages/Product/Product.vue
@@ -0,0 +1,1 @@
+<template>tere</template>
