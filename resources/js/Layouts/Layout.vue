509b7540b39b1422608a42036fc1a6d72a47af1b
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-10-21 10:22:50 +0300

    footer header

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen">
+        <nav class="h-20 w-full bg-gray-100" >test</nav>
+        <main class="flex-1">
+            <slot />
+        </main>
+        <footer class="h-20 w-full bg-gray-100">test</footer>
+    </div>
+</template>
---
+++ resources/js/Pages/Pood.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Layout from '@/Layouts/Layout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Layout>
+        tere
+    </Layout>
+</template>
