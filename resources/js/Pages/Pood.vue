f4c6d50b22ea7987b983993f8ac807b2bcf57b66
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-10-21 13:58:24 +0300

    cards

--- resources/js/Pages/Pood.vue
+++ resources/js/Pages/Pood.vue
@@ -5,6 +5,61 @@ import { Head } from '@inertiajs/inertia-vue3';
 
 <template>
     <Layout>
-        tere
+        <section class="bg-white py-8 ">
+
+            <div class=" mx-auto flex items-center flex-wrap max-w-screen p-12  ">
+    
+                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col ">
+                    <a href="#">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">Product Name</p>
+                        </div>
+                        <p class="pt-1 text-gray-900">£9.99</p>
+                    </a>
+                </div>
+
+                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+                    <a href="#">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">Product Name</p>
+                        </div>
+                        <p class="pt-1 text-gray-900">£9.99</p>
+                    </a>
+                </div>
+
+                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+                    <a href="#">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">Product Name</p>
+                        </div>
+                        <p class="pt-1 text-gray-900">£9.99</p>
+                    </a>
+                </div>
+
+                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+                    <a href="#">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">Product Name</p>
+                        </div>
+                        <p class="pt-1 text-gray-900">£9.99</p>
+                    </a>
+                </div>
+
+                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+                    <a href="#">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">Product Name</p>
+                        </div>
+                        <p class="pt-1 text-gray-900">£9.99</p>
+                    </a>
+                </div>
+            </div>
+    
+        </section>
     </Layout>
 </template>
