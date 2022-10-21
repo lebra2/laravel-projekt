c461af2dfde1cad7c993b9e6f50130cd4fb9b643
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-10-21 11:14:56 +0300

    Header normis nyyd

--- resources/js/Components/PageHeader.vue
+++ resources/js/Components/PageHeader.vue
@@ -5,12 +5,16 @@ import { Link } from '@inertiajs/inertia-vue3';
 
 <template>
     <nav class="w-full h-20 bg-gray-100">
-        <div class="max-w-screen h-full items-center flex flex-1 gap" >
-            <h1>E-pood</h1>
-            <div class="">
-                <Link href="#">Tooted</Link>
-                <Link href="#">Teised Tooted</Link>
-                <Link href="#">Tooted</Link>
+        <div class="max-w-screen-lg h-full items-center flex flex-1 gap-4 mx-auto w-full" >
+            <div class="flex gap-4 w-full justify-start text-yellow-900 text-xl cursor-pointer">
+                <Link class="hover:text-red-500" href="#"><p>Contact</p></Link>
+                <Link class="hover:text-red-500" href="#"><p>Products</p></Link>
+                <Link class="hover:text-red-500" href="#"><p>Stuff</p></Link>
+            </div>
+
+            <div class="flex gap-4 w-full justify-end text-yellow-900 text-xl cursor-pointer">
+                <Link class="hover:text-red-500" href="#"><p>Log in</p></Link>
+                <Link class="hover:text-red-500" href="#"><p>Register</p></Link>
             </div>
         </div>
 
