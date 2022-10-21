ad8a60d5a20b557e0c4b9131c0e16da915bcb1c1
Fredy Ait <aitfredy@gmail.com>
2022-10-21 11:19:21 +0300

    footer deisgn

--- resources/js/Components/PageFooter.vue
+++ resources/js/Components/PageFooter.vue
@@ -1,5 +1,16 @@
 <template>
-    <footer class="h-40 bg-gray-100 drop-shadow-md">
-        
+    <footer class="h-40 bg-gray-100 flex justify-evenly items-center">
+            <div>
+                <h1 class="font-bold text-xl">Contact Information</h1>
+                <p>Kohtu tn 12</p>
+                <a href="" class="underline">example@mail.com</a>
+                <p>+372 123 4567</p>
+            </div>
+            <div class="flex flex-col">
+                <h1 class="font-bold text-xl">Links</h1>
+                <a href="" class="underline">facebook.com</a>
+                <a href="" class="underline">instagram.com</a>
+                <a href="" class="underline">github.com</a>
+            </div>
     </footer>    
 </template>
